3e5fb00f6516427176516adc997d52cb5109b5b0
gopalbasak1 <gopalbasak1223@gmail.com>
2025-03-14 00:16:51 +0600

    admin dashboard eye icon remove

--- src/components/modules/admin/user/index.tsx
+++ src/components/modules/admin/user/index.tsx
@@ -154,14 +154,6 @@ const ManageUsersByAdmin = ({
       header: "Action",
       cell: ({ row }) => (
         <div className="flex items-center space-x-3">
-          <button
-            className="text-gray-500 hover:text-blue-500"
-            title="View"
-            onClick={() => handleView(row.original)}
-          >
-            <Eye className="w-5 h-5" />
-          </button>
-
           {row.original.isActive === true ? (
             <button
               className="text-gray-500 hover:text-yellow-500"
